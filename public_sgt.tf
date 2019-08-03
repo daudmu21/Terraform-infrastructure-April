b6761771c535bc524251fdfa16eb8ec5447c9a1a
daudmu21 <daudmuhudin@gmail.com>
2019-08-03 13:02:33 -0500

    add

--- public_sgt.tf
+++ public_sgt.tf
@@ -29,7 +29,6 @@ resource "aws_security_group" "Public" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
   }
 
   tags = {
