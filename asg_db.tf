78558a554db3bfed2bda3eefaa6ccb932597929c
daudmu21 <daudmuhudin@gmail.com>
2019-08-04 16:22:40 -0500

    edit and add

--- asg_db.tf
+++ asg_db.tf
@@ -1,27 +1,26 @@
-module "wordpress" {
+module "mysql" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "3.0"
-  
-  name = "wordpress"
+  name = "mysql"
 
   # Launch configuration
-  lc_name = "worpress-lc"
+  lc_name = "mysql-lc"
 
   image_id        = "${var.ami}"
   instance_type   = "${var.instance_type}"
-  security_groups = ["${aws_security_group.Public.id}"]
+  security_groups = ["${aws_security_group.PrivateDB.id}"]
 
   # Auto scaling group
-  asg_name                  = "worpress-asg"
-  vpc_zone_identifier       = ["${aws_subnet.public.id}"]
+  asg_name                  = "mysql-asg"
+  vpc_zone_identifier       = ["${aws_subnet.PrivateDB.id}"]
   health_check_type         = "EC2"
-  min_size                  = 3
+  min_size                  = 6
   max_size                  = 128
   desired_capacity          = 6
   wait_for_capacity_timeout = 0
 
   tags_as_map = {
-    Name = "${var.Name}"
+    Name = "${var.Name}.mqsl"
     Env = "${var.Env}"
     Created_by = "${var.Created_by}"
     Dept = "${var.Dept}"
